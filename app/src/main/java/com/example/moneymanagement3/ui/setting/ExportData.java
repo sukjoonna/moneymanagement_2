443c6b21a42a2cea06939c02c4c37313aed33801
Your Name <sukjoon216@gmail.com>
2020-07-27 17:29:59 -0400

    check

---
+++ app/src/main/java/com/example/moneymanagement3/ui/setting/ExportData.java
@@ -0,0 +1,148 @@
+package com.example.moneymanagement3.ui.setting;
+
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Color;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.text.InputType;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.Space;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AlertDialog;
+import androidx.core.content.FileProvider;
+import androidx.fragment.app.Fragment;
+
+import com.example.moneymanagement3.DataBaseHelper;
+import com.example.moneymanagement3.R;
+import com.example.moneymanagement3.ui.budget.SetBudgetCatFragment;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+
+import static java.time.temporal.ChronoUnit.DAYS;
+import static java.time.temporal.ChronoUnit.MONTHS;
+
+public class ExportData extends Fragment {
+    View view;
+    DataBaseHelper myDb;
+    Cursor res3; Cursor res4; Cursor res;
+    Button btn1; Button btn_export;
+    TextView tv_dates;
+    LocalDate startdate; LocalDate enddate;
+    DateTimeFormatter formatter;
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.fragment_export_data, container, false);
+        view.setBackgroundColor(Color.WHITE);
+
+
+        myDb = new DataBaseHelper(getActivity());
+        //Cursor res = gets all data in the database table3
+        res3 = myDb.get_setting();
+
+        //Variables
+        btn1 = view.findViewById(R.id.gobackBtn);
+        tv_dates = view.findViewById(R.id.dates);
+        btn_export = view.findViewById(R.id.btn_export);
+
+        formatter = DateTimeFormatter.ofPattern("LLL dd, yyyy");
+
+        //onstartup
+        res3.moveToFirst();
+        startdate = LocalDate.parse(res3.getString(0));
+        enddate = LocalDate.parse(res3.getString(1));
+        tv_dates.setText(startdate.format(formatter) + " ~ " + enddate.format(formatter));
+
+        onClick_ExportBtn();
+
+        //calling functions
+        onClick_GoBackBtn ();
+
+        return view;
+    }
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+    public void export() {
+        //generate data
+        StringBuilder data = new StringBuilder();
+        data.append("Time,Distance");
+        for (int i = 0; i < 5; i++) {
+            data.append("\n" + String.valueOf(i) + "," + String.valueOf(i * i));
+        }
+
+        try {
+            //saving the file into device
+            FileOutputStream out = getActivity().openFileOutput("data.csv", getActivity().MODE_PRIVATE);
+            out.write((data.toString()).getBytes());
+            out.close();
+
+            //exporting
+            Context context = getActivity().getApplicationContext();
+            File filelocation = new File(getActivity().getFilesDir(), "data.csv");
+            Uri path = FileProvider.getUriForFile(context, "com.example.exportcsv.fileprovider", filelocation);
+            Intent fileIntent = new Intent(Intent.ACTION_SEND);
+            fileIntent.setType("text/csv");
+            fileIntent.putExtra(Intent.EXTRA_SUBJECT, "Data");
+            fileIntent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+            fileIntent.putExtra(Intent.EXTRA_STREAM, path);
+            startActivity(Intent.createChooser(fileIntent, "Send mail"));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    public void onClick_ExportBtn () {
+        //Button to go back to settings
+        btn_export.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                export();
+            }
+        });
+    }
+
+
+    public void onClick_GoBackBtn () {
+        //Button to go back to settings
+        btn1.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                SettingFragment frag= new SettingFragment();
+                getActivity().getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.fragment_container, frag, "settingFrag")
+                        .commit();
+            }
+        });
+    }
+
+
+
+
+
+}
